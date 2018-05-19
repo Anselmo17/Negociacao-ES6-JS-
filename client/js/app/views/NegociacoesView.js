a406c9d75a198488ce03652222307b333c133c21
Anselmo <anselmo.dos.santos@avanade.com>
2018-05-19 09:04:36 -0300

    Create new modules

--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -4,7 +4,7 @@ constructor(elemento){
   this._elemento = elemento;
 }
 
-_template() {
+_template(model) {
 
   return `
   <table class="table table-hover table-bordered">
@@ -17,14 +17,35 @@ _template() {
                   </tr>
               </thead>
               <tbody>
+                ${model.negociacoes.map(n =>{
+                    return `
+                        <tr>
+                            <td>${DateHelper.dataParaTexto(n.data)}</td>
+                            <td>${n.quantidade}</td>
+                            <td>${n.valor}</td>
+                            <td>${n.volume}</td>
+                        </tr>
+                    `
+                  }).join('')}
               </tbody>
+              <tfoot>
+                  <td colspan="3"></td>
+                  <td>${
+                    (function(){
+                      let total = 0
+                      model.negociacoes.forEach(n => total += n.volume);
+                      return total; 
+                    })()
+
+                  }</td>
+              </tfoot>
        </table>
       `;
     }
 
   //convertendo o template em elemento do DOM
-  update(){
-    this._elemento.innerHTML = this._template();
+  update(model){
+    this._elemento.innerHTML = this._template(model);
   }
 
 }
