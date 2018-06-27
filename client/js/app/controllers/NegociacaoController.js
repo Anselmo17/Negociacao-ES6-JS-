ffdf2275cecd7f320ab4ba1dd888c5e3ed341711
Anselmo <anselmo.dos.santos@avanade.com>
2018-06-26 22:26:36 -0300

    Create modules

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,28 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        this._listaNegociacoes = new ListaNegociacoes();
+        
+        let self = this;
+
+        /* this._listaNegociacoes = new ListaNegociacoes((model)=>{
+            this._negociacoesView.update(model);
+        }); */
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(),{
+            
+            get(target , prop , receiver) {
+               
+                if(['adiciona','esvazia'].includes(prop) && typeof(target[prop] == typeof(Function))){
+
+                    return function(){
+                        console.log(`interceptando ${prop}`);
+                        Reflect.apply(target[prop], target , arguments);
+                        self._negociacoesView.update(target);
+                    }
+                }
+                return Reflect.get(target , prop , receiver);
+            }
+        });
+
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
         this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem = new Mensagem();
@@ -35,13 +56,11 @@ class NegociacaoController {
     }
   
     
-  apaga(e){
+  apaga(){
   
       //esvazia a lista de negociacoes 
       this._listaNegociacoes.esvazia();
-  
-      //atualiza a View
-      this._negociacoesView.update(this._listaNegociacoes)
+
   
       //Mensagem caso tenha sucesso
       this._mensagem.texto = 'Negociações apagadas com sucesso';
