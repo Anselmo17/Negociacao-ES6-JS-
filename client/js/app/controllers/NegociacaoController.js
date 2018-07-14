bb8ca364454c71b856d05df14c4c20ffddab0f81
Anselmo <anselmo.dos.santos@avanade.com>
2018-07-14 09:42:08 -0300

    Create modules

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,86 +1,69 @@
 class NegociacaoController {
-    
+
     constructor() {
-        
+
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-        
-        let self = this;
 
-        /* this._listaNegociacoes = new ListaNegociacoes((model)=>{
-            this._negociacoesView.update(model);
-        }); */
-        this._listaNegociacoes = new Proxy(new ListaNegociacoes(),{
-            
-            get(target , prop , receiver) {
-               
-                if(['adiciona','esvazia'].includes(prop) && typeof(target[prop] == typeof(Function))){
+        this._listaNegociacoes = new Bind (
+                new ListaNegociacoes(),
+                 new NegociacoesView($('#negociacoesView')),
+                'adiciona', 'esvazia');
 
-                    return function(){
-                        console.log(`interceptando ${prop}`);
-                        Reflect.apply(target[prop], target , arguments);
-                        self._negociacoesView.update(target);
-                    }
-                }
-                return Reflect.get(target , prop , receiver);
-            }
-        });
-
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem);
+        this._mensagem = new Bind(
+            new Mensagem(),
+            new MensagemView($('#mensagemView')),
+            'texto');
     }
-    
+
     adiciona(event) {
-        
+
         event.preventDefault();
-  
+
         //adicionando novos dados
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-  
+
         //mensagem de adicionado com sucesso
         this._mensagem.texto = "Negociação adicionada com sucesso";
-  
+
         //mesangem exposta na tela 
-        this._mensagemView.update(this._mensagem);
-  
+        this._mensagem.update(this._mensagem);
+
         //atualizando a lista de negociacão
         this._negociacoesView.update(this._listaNegociacoes);
-  
-        //limpa o formulario
-        this._limpaFormulario();   
-    }
-  
-    
-  apaga(){
-  
-      //esvazia a lista de negociacoes 
-      this._listaNegociacoes.esvazia();
 
-  
-      //Mensagem caso tenha sucesso
-      this._mensagem.texto = 'Negociações apagadas com sucesso';
-      this._mensagemView.update(this._mensagem)
-  }
-  
-  
+        //limpa o formulario
+        this._limpaFormulario();
+    }
+
+
+    apaga() {
+
+        //esvazia a lista de negociacoes 
+        this._listaNegociacoes.esvazia();
+
+
+        //Mensagem caso tenha sucesso
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem)
+    }
+
+
     _criaNegociacao() {
-        
+
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value);    
+            this._inputValor.value);
     }
-    
+
     _limpaFormulario() {
-     
+
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-        this._inputData.focus();   
+        this._inputData.focus();
     }
-  }
+}
